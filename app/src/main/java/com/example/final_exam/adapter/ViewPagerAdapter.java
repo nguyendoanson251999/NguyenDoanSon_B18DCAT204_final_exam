f328fa999173ef32d0466de0649196a6c0302013
sonnd <57580701+nguyendoanson251999@users.noreply.github.com>
2022-05-28 01:02:25 +0700

    applogshoping

---
+++ app/src/main/java/com/example/final_exam/adapter/ViewPagerAdapter.java
@@ -0,0 +1,32 @@
+package com.example.final_exam.adapter;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+import com.example.final_exam.fragment.FragmentHistory;
+import com.example.final_exam.fragment.FragmentHome;
+import com.example.final_exam.fragment.FragmentSearch;
+
+public class ViewPagerAdapter extends FragmentStatePagerAdapter {
+    public ViewPagerAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:return new FragmentHome();
+            case 1:return new FragmentHistory();
+            case 2:return new FragmentSearch();
+        }
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+}
